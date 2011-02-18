c7dbf60abc702dfaa5039e450308706500f5a716
Martynas Budriūnas <martynas@budriunas.lt>
2011-02-18 13:53:15 +0200

    Nedideli atnaujinimai

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,9 +1,5 @@
 INSERT INTO Modeliai(Pavadinimas, Koeficientas) SELECT * FROM CSVREAD('modeliai.tsv', NULL, 'UTF-8', chr(9));
 INSERT INTO Regatos (Pavadinimas, Regionas) VALUES ('Pilypo regata', 'Vakarų Lietuvos');
---INSERT INTO Etapai (Regata, Metai, Numeris) VALUES (1, 2010, 1), (1, 2010, 2), (1, 2010, 3);
-INSERT INTO Etapai (Regata, Metai, Numeris) VALUES (1, 2010, 1);
-INSERT INTO Etapai (Regata, Metai, Numeris) VALUES (1, 2010, 2);
-INSERT INTO Etapai (Regata, Metai, Numeris) VALUES (1, 2010, 3);
 --INSERT INTO Kapitonai (Vardas) VALUES ('Kapitonas Kapitonauskas'), ('Kapitonė Kapitonienė');
 INSERT INTO Kapitonai (Vardas) VALUES ('Kapitonas Kapitonauskas');
 INSERT INTO Kapitonai (Vardas) VALUES ('Kapitonė Kapitonienė');
@@ -13,7 +9,3 @@ INSERT INTO Savininkai (Vardas) VALUES ('Savininkė Savininkienė');
 --INSERT INTO Jachtos (BurėsNumeris, Modelis, Pavadinimas, Kapitonas, Savininkas) VALUES ('LTU2018', 314, 'Gafsan', 1, 2), ('LTU1547', 271, 'Piranija', 2, 1);
 INSERT INTO Jachtos (BurėsNumeris, Modelis, Pavadinimas, Kapitonas, Savininkas) VALUES ('LTU2018', 314, 'Gafsan', 1, 2);
 INSERT INTO Jachtos (BurėsNumeris, Modelis, Pavadinimas, Kapitonas, Savininkas) VALUES ('LTU1547', 271, 'Piranija', 2, 1);
-INSERT INTO Plaukimai (Jachta, Etapas, StartoLaikas, FinišoLaikas, Kapitonas, Savininkas) VALUES
-  ('LTU2018', 1, '2010-12-06 8:30:00', '2010-12-06 9:50:00', 1, 2);
-INSERT INTO Plaukimai (Jachta, Etapas, StartoLaikas, FinišoLaikas, Kapitonas, Savininkas) VALUES
-  ('LTU1547', 1, '2010-12-06 8:40:00', '2010-12-06 9:20:00', 2, 1);
